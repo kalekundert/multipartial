427c80a47aae4649ae5484c3575bddd2f2c33f19
Kale Kundert <kale@thekunderts.net>
2024-11-08 14:19:00 -0500

    feat: add require functions to top-level namespace

--- multipartial/__init__.py
+++ multipartial/__init__.py
@@ -5,6 +5,7 @@ N-dimensional arrays of partial functions.
 __version__ = '0.1.0'
 
 from .api import (
-    multipartial, partial_list, partial_grid,
-    dim, Dim, rows, cols, put,
+        multipartial, partial_list, partial_grid,
+        dim, Dim, rows, cols, put,
 )
+from .require import require_list, require_grid, require_array
